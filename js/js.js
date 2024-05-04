7e7e7c79de3cd73509a095133a6cb10bd6c23fd6
yohanbarbosa <yohanbar30@gmail.com>
2024-05-04 15:47:14 -0500

    json construido

--- js/js.js
+++ js/js.js
@@ -1,50 +1,189 @@
 
+
+let usuarios = [];
+
+// Inicializar usuarios si ya existe en localStorage
+const storedData = localStorage.getItem('userProfiles');
+if (storedData) {
+  usuarios = JSON.parse(storedData);
+}
 const formulario = document.getElementById('user-profile-form');
+formulario.addEventListener('submit', (evento) => {
+  evento.preventDefault(); 
 
-    formulario.addEventListener('submit', (evento) => {
-      evento.preventDefault();
-      const nombre = document.getElementById('name').value.trim();
-      const edad = parseInt(document.getElementById('age').value);
-      const peso = parseFloat(document.getElementById('weight').value);
-      const altura = parseFloat(document.getElementById('height').value);
-      const género = document.getElementById('gender').value;
-  
-      // Validar datos
-      if (isNaN(edad) || edad <= 0) {
-        alert('Edad inválida.');
-        return;
-      }
+  const datos_pagina = document.getElementById('btn').value;
 
-      if (isNaN(peso) || peso <= 0) {
-        alert('Peso inválido.');
-        return;
-      }
-
-      if (isNaN(altura) || altura <= 0) {
-        alert('Altura inválida.');
-        return;
-      }
-
-      const datosUsuario = {
-        nombre: nombre,
-        edad: edad,
-        peso: peso.toFixed(2), // Redondear peso a dos decimales
-        altura: altura.toFixed(2), // Redondear altura a dos decimales
-        género: género
-      };
-
-      localStorage.setItem('userProfile', JSON.stringify(datosUsuario));
-      alert('¡Perfil guardado correctamente!');
-      window.location.href = 'grupomuscular.html'; // Redirigir a la página planeacion
-    });
-
-    // recuperación y uso del perfil almacenado
-    const perfilAlmacenado = localStorage.getItem('userProfile');
-    if (perfilAlmacenado) {
-      const perfil = JSON.parse(perfilAlmacenado);
-      console.log('Perfil recuperado:');
-      console.log('Edad:', perfil.edad);
-      console.log('Peso:', perfil.peso);
-      console.log('Altura:', perfil.altura);
-      console.log('Género:', perfil.género);
+  if (datos_pagina == "perfil") {
+    // ... código para manejar la página de perfil ...
+       
+    const nombre = document.getElementById('name').value.trim();
+    const edad = parseInt(document.getElementById('age').value);
+    const peso = parseFloat(document.getElementById('weight').value);
+    const altura = parseFloat(document.getElementById('height').value);
+    const género = document.getElementById('gender').value;
+  //  Validar datos
+    
+    if (isNaN(edad) || edad <= 0) {
+      alert('Edad inválida.');
+      return;
     }
+    
+    if (isNaN(peso) || peso <= 0) {
+    alert('Peso inválido.');
+    return;
+  }
+  
+  if (isNaN(altura) || altura <= 0) {
+    alert('Altura inválida.');
+    return;
+  }
+    const perfil = {
+      nombre: nombre,
+      edad: edad,
+      peso: peso.toFixed(2), // Redondear peso a dos decimales
+      altura: altura.toFixed(2), // Redondear altura a dos decimales
+      género: género,
+      
+    };
+    
+    // Verificar si el perfil ya existe en la lista de perfiles
+    const usuarioExistente = usuarios.find(usuario => usuario.nombre === perfil.nombre);
+    if (usuarioExistente) {
+      alert("El perfil ya existe");
+      return;
+    }
+    
+    usuarios.push(perfil);
+    localStorage.setItem('userProfiles', JSON.stringify(usuarios));
+    alert("data save");
+    window.location.href = 'objetivo.html';
+
+  } else if (datos_pagina == "objetivo") {
+    // ... código para manejar la página de objetivo ...
+
+    const opcion_objetivo = [];
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    
+    checkboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+        opcion_objetivo.push(checkbox.id);
+      }
+    });
+    
+    // Asegúrate de que el último usuario agregado existe y es un objeto
+    if (usuarios[usuarios.length - 1] && typeof usuarios[usuarios.length - 1] === 'object') {
+      usuarios[usuarios.length - 1].objetivo = opcion_objetivo;
+    }
+    
+    localStorage.setItem('userProfiles', JSON.stringify(usuarios));
+    window.location.href = 'lugarentrenamiento.html';
+
+  }else if(datos_pagina == "lugar"){           // ... código para manejar la página de lugar de entrenamiento ...
+ 
+
+ const opcion_lugar = [];
+ const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+ 
+ checkboxes.forEach(checkbox => {
+   if (checkbox.checked) {
+    opcion_lugar.push(checkbox.id);
+   }
+ });
+ 
+ // condicion para revisar que el último usuario agregado existe y es un objeto
+ if (usuarios[usuarios.length - 1] && typeof usuarios[usuarios.length - 1] === 'object') {
+   usuarios[usuarios.length - 1].lugar = opcion_lugar;
+ }
+ localStorage.setItem('userProfiles', JSON.stringify(usuarios));
+ window.location.href = "grupomuscular.html";
+
+
+
+  }else if(datos_pagina == "musculos"){                // ... código para manejar la página de grupo muscular ...
+    
+
+ const opcion_musculos = [];
+ const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+ 
+ checkboxes.forEach(checkbox => {
+   if (checkbox.checked) {
+    opcion_musculos.push(checkbox.id);
+   }
+ });
+ 
+ // condicion para revisar que el último usuario agregado existe y es un objeto
+ if (usuarios[usuarios.length - 1] && typeof usuarios[usuarios.length - 1] === 'object') {
+   usuarios[usuarios.length - 1].musculos = opcion_musculos;
+ }
+ 
+ localStorage.setItem('userProfiles', JSON.stringify(usuarios));
+ window.location.href = "experiencia.html";
+
+  }else if(datos_pagina == "experiencia"){                // ... código para manejar la página de experiencia en el gimnasio ...
+    
+
+    const opcion_experiencia = [];
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    
+    checkboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+        opcion_experiencia.push(checkbox.id);
+      }
+    });
+    
+    // condicion para revisar que el último usuario agregado existe y es un objeto
+    if (usuarios[usuarios.length - 1] && typeof usuarios[usuarios.length - 1] === 'object') {
+      usuarios[usuarios.length - 1].experiencia = opcion_experiencia;
+    }
+    
+    localStorage.setItem('userProfiles', JSON.stringify(usuarios));
+    window.location.href = "dias.html";
+    
+     }else if(datos_pagina == "dias"){                // ... código para manejar la página de dias de entrenamiento ...
+    
+
+      const opcion_dias = [];
+      const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+      
+      checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+          opcion_dias.push(checkbox.id);
+        }
+      });
+      
+      // condicion para revisar que el último usuario agregado existe y es un objeto
+      if (usuarios[usuarios.length - 1] && typeof usuarios[usuarios.length - 1] === 'object') {
+        usuarios[usuarios.length - 1].dias = opcion_dias;
+      }
+      
+      localStorage.setItem('userProfiles', JSON.stringify(usuarios));
+    
+      console.log(usuarios);
+     
+       }
+     
+});
+
+
+// const limpiar = document.getElementById('limpiar');
+
+// limpiar.addEventListener('click', function borrar(){
+
+//   localStorage.removeItem('userProfiles');
+//    console.log("borrado done");
+   
+//    window.location.href = "index.html";
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
