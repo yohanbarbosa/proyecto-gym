fd8fe8637f1a13e4d9a722c1042fe78000085e60
yohanbarbosa <yohanbar30@gmail.com>
2024-05-09 18:32:26 -0500

    actualizacion biblioteca

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,70 +1,16 @@
-//########################################### CONTADOR ####################################################################################
-
-let valuedisplay = document.querySelectorAll(".numero");
-let interval = 1500;
-
-
-valuedisplay.forEach((valuedisplay) => {
-let starvalue = 0;
-let endvalue = parseInt(valuedisplay.getAttribute
-("data-val"));
-let duration = Math.floor(interval / endvalue);
-let counter = setInterval(function(){
-    starvalue += 1;
-    valuedisplay.textContent = starvalue;
-    if(starvalue == endvalue){
-        clearInterval(counter);
-    }
-} , duration);
-
-
-});
-
-
-//################################################ menu hamburguesa ######################################################
-let enlaces_header = document.querySelectorAll(".navegation")[0];
-let icon_hamburguer = true;
-
-
-document.querySelectorAll(".menu")[0].addEventListener("click", function(){
-    
-    if(icon_hamburguer){
-        document.querySelectorAll(".menu")[0].style.color="#f05d07";
-        icon_hamburguer = false;
-
-    }else{
-        document.querySelectorAll(".menu")[0].style.color="#fff";
-        icon_hamburguer = true;
-    } 
-
-    enlaces_header.classList.toggle("MenuOpen");
-
-});
-
 
 
 //################################################ LOGIN ##################################################################################
 
 const wrapper  = document.querySelector('.wrapper');
-const loginlink  = document.querySelector('.login_link');
-const resgitrolink  = document.querySelector('.registre_link');
-const btnpopup  = document.getElementById('buttonbtn');
-const iconclose  = document.querySelector('.icon_close');
+const btn_activado  = document.getElementById('btn_activar');
+const iconclose = document.querySelector('.icon_close');
 
 
-resgitrolink.addEventListener('click',()=> {
-wrapper.classList.add('active');
-wrapper.style.height = "75vh";
-});
-
-loginlink.addEventListener('click',()=> {
-    wrapper.classList.remove('active');
-    wrapper.style.height = "70vh";
-    });
-
-    btnpopup.addEventListener('click',()=> {
+    btn_activado.addEventListener('click',()=> {
+    
         wrapper.classList.add('active_btn');
-        enlaces_header.classList.remove('MenuOpen');
+      
     });
 
     iconclose.addEventListener('click',()=> {
